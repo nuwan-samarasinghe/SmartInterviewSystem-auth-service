034644a3856fccb877881ebd3e33408dbcb13868
nuw1991 <nuwansamarasinghe100@gmail.com>
2020-06-06 09:05:38 +0530

    adding oauth2 user authentication

--- src/main/java/com/smartinterviewshedular/authservice/AuthServiceApplication.java
+++ src/main/java/com/smartinterviewshedular/authservice/AuthServiceApplication.java
@@ -2,10 +2,12 @@ package com.smartinterviewshedular.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@EntityScan(basePackages = {"com.smartinterviewshedular.commonlib.auth.model"})
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
