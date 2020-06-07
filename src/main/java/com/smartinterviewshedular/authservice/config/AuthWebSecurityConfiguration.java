13cb71f6594ba0e5ec87b9dcac6117c7e6ffce69
nuwansamarasinghe100@gmail.com <sama@272199360G>
2020-06-07 13:40:47 +0530

    adding custom login

--- src/main/java/com/smartinterviewshedular/authservice/config/AuthWebSecurityConfiguration.java
+++ src/main/java/com/smartinterviewshedular/authservice/config/AuthWebSecurityConfiguration.java
@@ -22,7 +22,7 @@ public class AuthWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/webjars/**").permitAll()
+                .antMatchers("/webjars/**", "/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll();
--- src/main/java/com/smartinterviewshedular/authservice/controller/LoginController.java
+++ src/main/java/com/smartinterviewshedular/authservice/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.smartinterviewshedular.authservice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -11,5 +12,11 @@ public class LoginController {
     String loginUser() {
         return "login";
     }
+
+    @GetMapping("/register")
+    String registerUser() {
+        return "register";
+    }
+
 
 }
