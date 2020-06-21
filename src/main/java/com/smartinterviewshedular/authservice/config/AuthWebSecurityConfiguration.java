0402ea5f7e7a3cf8d43b6b4238cd0c132dc97bcc
nuwansamarasinghe100@gmail.com <sama@272199360G>
2020-06-22 01:57:29 +0530

    adding cross origin

--- src/main/java/com/smartinterviewshedular/authservice/config/AuthWebSecurityConfiguration.java
+++ src/main/java/com/smartinterviewshedular/authservice/config/AuthWebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.smartinterviewshedular.authservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @Configuration("authWebSecurityConfiguration")
 public class AuthWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -21,8 +23,11 @@ public class AuthWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/webjars/**", "/register", "/forgot-password" , "/images/**").permitAll()
+        http
+                .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "**").permitAll()
+                .antMatchers("/webjars/**", "/register", "/forgot-password", "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").failureUrl("/login?error=true").permitAll();
